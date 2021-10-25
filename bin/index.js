1de27ce6d8bf30529c1fc63b7dc001648c4c7b30
matepak <matszczu@gmail.com>
2021-10-26 00:06:15 +0200

    added function renderClock, which outputs digits of clock based on input string

--- bin/index.js
+++ bin/index.js
@@ -1,4 +1,24 @@
 #!/usr/bin/env node
 
-console.log("world clock app...")
-console.log("tick tok")
+const clear = require('clear');
+const fonts = require('../fonts/fonts.json');
+let argv = require('minimist')(process.argv.slice(2));
+
+function clock(timeZone) {
+    clear();
+    renderClock("00 : 00");
+    console.log('press ctrl+c to exit...');
+}
+
+function renderClock(time) {
+    for (let j = 0; j < 5; j++) {
+        for (let k = 0; k < time.length; k++) {
+            process.stdout.write(fonts.chars[time[k]][j]);
+        }
+        process.stdout.write('\n');
+    }
+    return clock;
+
+};
+
+let clockIntervalId = setInterval(clock, 1000);
