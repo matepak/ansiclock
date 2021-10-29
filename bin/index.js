557ffbd2aab69e4fcc56200b029cdd68e6436290
matepak <matszczu@gmail.com>
2021-10-29 10:39:00 +0200

    fixed screen flickering

--- bin/index.js
+++ bin/index.js
@@ -4,21 +4,39 @@ const clear = require('clear');
 const fonts = require('./fonts.json');
 let argv = require('minimist')(process.argv.slice(2));
 
+const drawLine = (line) => {
+    for (let i = 0; i < line.length; i++) {
+        if (line[i] === '0') process.stdout.write(' ');
+        if (line[i] === '1') {
+            process.stdout.write('\x1b[2;37;41m ');
+            process.stdout.write('\x1b[0m');
+        }
+
+    }
+}
+clear();
+process.stderr.write('\x1B[?25l');
+
 function clock(timeZone) {
-    clear();
-    renderClock("00 : 00");
-    console.log('press ctrl+c to exit...');
+    renderClock(getTime());
+}
+process.stdout.clearScreenDown();
+function getTime() {
+    return [...Date().slice(16, 21)].map(char => char + ' ').join('');
 }
 
 function renderClock(time) {
     for (let j = 0; j < 5; j++) {
         for (let k = 0; k < time.length; k++) {
-            process.stdout.write(fonts.chars[time[k]][j]);
+            //process.stdout.clearLine(1);
+            drawLine(fonts.chars[time[k]][j]);
         }
         process.stdout.write('\n');
     }
-    return clock;
-
+    process.stdout.cursorTo(0, 0);
 };
+
+
+
 
 let clockIntervalId = setInterval(clock, 1000);
